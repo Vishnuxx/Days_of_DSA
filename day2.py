28bc2217ab2b1574c84785428e237386de8af384
vishnuxx <vishnuvs2002it@gmail.com>
2022-06-26 19:54:13 +0530

    day5

--- day2.py
+++ day2.py
@@ -1,3 +1,6 @@
+#Q) Given an array nums of integers, return how many of them contain an even number of digits.
+# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
 array = [12 , 134 , 132434 , 122 , 2323 , 23,1, 2244]
 
 for i in array:
